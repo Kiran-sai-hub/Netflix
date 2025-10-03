158f949ecafed89e112be9498f4cdc2d48ee8e83
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2025-10-04 02:08:09 +0530

    Finished Backend, and basic Frontend skeleton...

---
+++ api/controllers/search.controller.js
@@ -0,0 +1,138 @@
+import User from "../models/user.model.js";
+import { fetchFromTmdb } from "../services/tmdb.service.js";
+
+const searchPerson = async (req, res) => {
+  const { query } = req.params;
+  if (!query) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Query parameter is required" });
+  }
+  try {
+    const url = `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`;
+    const response = await fetchFromTmdb(url);
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].profile_path,
+          title: response.results[0].name,
+          searchType: "person",
+          createdAt: new Date(),
+        },
+      },
+    });
+
+    res.status(200).json({ success: true, content: response.results });
+  } catch (error) {
+    console.log(`Error in searchPerson Controller: ${error.message}`);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const searchMovie = async (req, res) => {
+  const { query } = req.params;
+  if (!query) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Query parameter is required" });
+  }
+  try {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+    const response = await fetchFromTmdb(url);
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].poster_path,
+          title: response.results[0].title,
+          searchType: "movie",
+          createdAt: new Date(),
+        },
+      },
+    });
+
+    res.status(200).json({ success: true, content: response.results });
+  } catch (error) {
+    console.log(`Error in searchMovie Controller: ${error.message}`);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const searchTv = async (req, res) => {
+  const { query } = req.params;
+  if (!query) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Query parameter is required" });
+  }
+  try {
+    const url = `https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`;
+    const response = await fetchFromTmdb(url);
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].poster_path,
+          title: response.results[0].name,
+          searchType: "tv",
+          createdAt: new Date(),
+        },
+      },
+    });
+
+    res.status(200).json({ success: true, content: response.results });
+  } catch (error) {
+    console.log(`Error in searchTv Controller: ${error.message}`);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const getSearchHistory = async (req, res) => {
+  try {
+    res.status(200).json({ success: true, content: req.user.searchHistory });
+  } catch (error) {
+    console.log(`Error in getSearchHistory Controller: ${error.message}`);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const removeItemFromSearchHistory = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res
+      .status(400)
+      .json({ success: false, message: "ID parameter is required" });
+  }
+  try {
+    await User.findByIdAndUpdate(req.user._id, {
+      $pull: {
+        searchHistory: { id: Number(id) },
+      },
+    });
+    res.status(200).json({ success: true, message: "Item removed from search history" });
+  } catch (error) {
+    console.log(`Error in removeItemFromSearchHistory Controller: ${error.message}`);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export {
+  searchPerson,
+  searchMovie,
+  searchTv,
+  getSearchHistory,
+  removeItemFromSearchHistory,
+};
---
+++ api/middleware/protectRoute.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import { ENV_VARS } from "../config/envVars.js";
+import User from "../models/user.model.js";
+
+const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt || "";
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: No token Provided" });
+    }
+
+    const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
+    if (!decoded) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: Invalid Token" });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Unauthorized: No Permission" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(`Error in protectRoute middleware: ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+export default protectRoute;
