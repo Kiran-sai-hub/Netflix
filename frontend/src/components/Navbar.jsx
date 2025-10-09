21590e90e04c4ded5dffc2a7652a852a74ad4e51
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2025-10-10 00:08:41 +0530

    Completed the Entire Homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Search, LogOut, Menu } from "lucide-react"
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/authUser.js"
+import { useContentStore } from "../store/content.js";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const { user, logout } = useAuthStore();
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const { setContentType } = useContentStore();
+
   return (
     <header className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20">
       <div className="flex items-center gap-10 z-50">
@@ -10,21 +23,48 @@ const Navbar = () => {
         </Link>
 
         <div className="hidden sm:flex gap-2 items-center">
-          <Link to="/" className="hover:text-red-500 m-1">
+          <Link to="/" className="hover:text-gray-300 m-1" onClick={() => setContentType("movie")}>
             Movies
           </Link>
-          <Link to="/" className="hover:text-red-500 m-1">
+          <Link to="/" className="hover:text-gray-300 m-1" onClick={() => setContentType("tv")}>
             TV Shows
           </Link>
-          <Link to="/history" className="hover:text-red-500 m-1">
+          <Link to="/history" className="hover:text-gray-300 m-1">
             Search History
           </Link>
         </div>
       </div>
 
-      <div>
-        
+      <div className='flex gap-2 items-center z-50'>
+        <Link to={"/search"}>
+          <Search className="size-6 cursor-pointer"/>
+        </Link>
+        <img src={user.image} alt='Avatar' className='h-8 rounded cursor-pointer' />
+        <LogOut className="size-6 cursor-pointer" onClick={logout}/>
+        <div className="sm:hidden">
+          <Menu className="size-6 cursor-pointer" onClick={toggleMobileMenu}/>
+        </div>
       </div>
+
+      {isMobileMenuOpen && (
+        <div className='w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-800'>
+          <Link to={"/"} className='block hover:text-gray-300 p-2' onClick={() => {
+            setContentType("movie");
+            toggleMobileMenu;
+            }}>
+						Movies
+					</Link>
+          <Link to={"/"} className='block hover:text-gray-300 p-2' onClick={() => {
+            setContentType("tv");
+            toggleMobileMenu;
+            }}>
+						Tv Shows
+					</Link>
+					<Link to={"/history"} className='block hover:text-gray-300 p-2' onClick={toggleMobileMenu}>
+						Search History
+					</Link>
+        </div>
+      )}
     </header>
   );
 };
